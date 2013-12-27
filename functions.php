2353a5c9213885cc5dedf2a1f6f8b4de7151d963
mortenjust <morten.just@genstart.dk>
2013-12-26 21:04:22 -0800

    stuff for connecting to the database

---
+++ functions.php
@@ -0,0 +1,85 @@
+<?
+$db = mysql_connect("localhost", "wgenstar_somapus", "3SqIawG3OFamTs9Gj");
+mysql_select_db ("wgenstar_somapus", $db); 
+  
+ 
+   function f($hvad) {  
+        #return mb_convert_case($hvad, MB_CASE_TITLE);  
+        return $hvad;
+     }
+  
+  function h($hvad) {  
+      #return utf8_encode($hvad);
+      return $hvad;
+    }
+
+   function format_time ($t) { 
+      $tid=strtotime($t);
+      return date("G:i", $tid." ");
+          
+     }
+     
+     function snippet($text,$length=64,$tail="...") {
+        $text = trim($text);
+        $txtl = strlen($text);
+        if($txtl > $length) {
+            for($i=1;$text[$length-$i]!=" ";$i++) {
+                if($i == $length) {
+                    return substr($text,0,$length) . $tail;
+                }
+            }
+            $text = substr($text,0,$length-$i+1) . $tail;
+        }
+        return $text;
+     }
+
+
+
+
+     function u($hvad) {  
+       return $hvad;
+       }
+  
+     function split_url ($url) {
+       preg_match('`<a\s+href=\"([^"]*)\">(?!<img[^>]*>)(.*)</a>`i',$url, $matches);     
+       return $matches; # 0 er originalen i html, 1 er url, 2 er linktekst
+        }   
+        
+      function close_db($which){    
+        mysql_close($which);
+      } 
+
+
+	 function ConvertMinutes2Hours($Minutes)
+	{
+	    if ($Minutes < 0)
+	    {
+	        $Min = Abs($Minutes);
+	    }
+	    else
+	    {
+	        $Min = $Minutes;
+	    }
+	    $iHours = Floor($Min / 60);
+	    $Minutes = ($Min - ($iHours * 60)) / 100;
+	    $tHours = $iHours + $Minutes;
+	    if ($Minutes < 0)
+	    {
+	        $tHours = $tHours * (-1);
+	    }
+	    $aHours = explode(".", $tHours);
+	    $iHours = $aHours[0];
+	    if (empty($aHours[1]))
+	    {
+	        $aHours[1] = "00";
+	    }
+	    $Minutes = $aHours[1];
+	    if (strlen($Minutes) < 2)
+	    {
+	        $Minutes = $Minutes ."0";
+	    }
+	    $tHours = $iHours ." time ". $Minutes. " minutter";
+	    return $tHours;
+	}
+    
+  ?>
---
+++ index.php
@@ -0,0 +1,13 @@
+<?
+
+$encoding = "utf-8";
+require ("functions.php");
+
+$sql = "insert replace into somapus (updated, room, location, user) values ()";	
+mysql_query($sql);
+
+
+
+
+
+?>
