f086f1e33d68dcf349da75a36406e939c7c0575a
mortenjust <morten.just@genstart.dk>
2013-12-28 19:30:34 -0800

    no map margins, no controls

--- map.php
+++ map.php
@@ -4,8 +4,8 @@
   <head>
     <title>So map us!</title>
     <style>
-      body,html {height:100%}
-      #map {width:100%;height:100%;}
+      body,html {height:100%; padding:0; margin:0;}
+      #map {width:100%;height:100%;}    
     </style>
 	<script src="https://www.google.com/jsapi"></script>
   <script src="mapstyle.js"></script>
@@ -29,9 +29,15 @@
             maximumAge:0
           });
           
-          var myOptions = {
+       var myOptions = {
             zoom: 15,
+            panControl: false, 
+            zoomControl: false, 
+            streetViewControl: false, 
+            overviewMapControl: false,
+            mapTypeControl: false,
             styles: mapStyles,
+            center: myLatlng,
             mapTypeId: google.maps.MapTypeId.ROADMAP
           }
           map = new google.maps.Map(document.getElementById("map"), myOptions);
